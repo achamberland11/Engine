3e842909b5c5f07749ac9f61db4a9b5e3f0448ab
Virgile <112096529+achamberland11@users.noreply.github.com>
2026-03-29 03:27:44 -0400

    Engine -> Game
    - Il faut maintenant utiliser le projet Game pour build. Engine est maintenant une librairie statique

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,6 +1,25 @@
 
-int main(int argc, char* argv[])
+#include "../Engine/src/Core/GameEngine.h"
+
+void Start()
 {
-    
+    CGameEngine::Instance().Init();
+}
+
+void Loop()
+{
+    while (!CGameEngine::Instance().ShouldQuit())
+    {
+        CGameEngine::Instance().Loop();
+    }
+
+    CGameEngine::Instance().Shutdown();
+}
+
+int main(int argc, char *argv[])
+{
+    Start();
+    Loop();
+
     return 0;
 }
